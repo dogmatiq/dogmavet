23e713d6849a68d7208b3aadb77f623f6a0d9a01
James Harris <mailjamesharris@gmail.com>
2020-05-03 17:14:19 +1000

    Ensure dogma stays in the go.mod file.

---
+++ passes/dogma_test.go
@@ -0,0 +1,5 @@
+package passes_test
+
+// Ensure dogma stays in go.mod. It is needed by the Makefile to setup the
+// temporary GOPATH needed by the analysistest package.
+import _ "github.com/dogmatiq/dogma"
